ebee371779ca58f962eed9423ed7dde99526aefa
Dean <mccarthey123@outlook.com>
2018-04-26 19:41:15 +0800

    删除初始化数组

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <draggable v-model="myArray" >
       <transition-group>
         <div v-for="element in myArray" :key="element.id" class="draggable-item">
-          <mu-icon value="label_outline" :color="element.color" />
+          <mu-icon value="label_outline" :color="RandomColor" />
           <mu-text-field v-model="element.text" hintText="写点儿什么吧" @blur="onSave" /><br/>
         </div>
       </transition-group>
@@ -25,21 +25,8 @@ export default {
   components: { draggable },
   data() {
     return {
-      myArray: [
-        {
-          id: 1,
-          name: 'demo1'
-        },
-        {
-          id: 2,
-          name: 'demo2'
-        },
-        {
-          id: 3,
-          name: 'demo3'
-        }
-      ],
-      RandomColor: 'red'
+      myArray: [],
+      RandomColor: '#999'
     }
   },
   created(){
