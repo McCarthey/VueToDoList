26e5ff97e368afc9eff3fde673b3ca6138561970
Dean <mccarthey123@outlook.com>
2018-06-14 09:35:11 +0800

    引入offline-plugin

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
 import 'muse-ui/dist/theme-light.css' // 使用 XXX 主题
 import colorPicker from './vue-color-picker' //引入colorPicker组件
+import * as OfflinePluginRuntime from 'offline-plugin/runtime'; // 引入offline-plugin插件
+OfflinePluginRuntime.install();
 
 Vue.use(MuseUI)
-// 使用colorPicker组件
+    // 使用colorPicker组件
 Vue.use(colorPicker)
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
